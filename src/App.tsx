873e34f46d2e4af50dcb61f5e37aba6b1e794ae9
Ritin George <78461388+aurora-0025@users.noreply.github.com>
2022-11-12 18:24:51 +0530

    Initial Commit 😎

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Events from './pages/events/Events';
+import Home from './pages/home/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="events" element={<Events />} />
+                <Route path="events" element={<Events />} />
+                <Route path="*" element={<p>404</p>} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/oppurtunities/Oppurtunities.tsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div>
+            <h1>This is the oppurtunities page</h1>
+        </div>
+    );
+}
+
+export default Home;
