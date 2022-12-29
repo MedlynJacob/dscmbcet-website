6a5be5bb178fc8443bbf52af7c49f343d37a48f7
Liya Susan Biju <liyasusanbiju@gmail.com>
2022-12-30 02:26:13 +0530

    updated commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Events from './pages/events/Events';
 import Home from './pages/home/Home';
+import Oppurtunities from './pages/oppurtunities/Oppurtunities';
+import Resources from './pages/resources/Resources';
 
 function App() {
     return (
         <div className="App">
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="events" element={<Events />} />
-                <Route path="events" element={<Events />} />
+                <Route path="/events" element={<Events />} />
+                <Route path="/resources" element={<Resources />} />
+                <Route path="/oppurtunities" element={<Oppurtunities />} />
                 <Route path="*" element={<p>404</p>} />
             </Routes>
         </div>
