7cf62bf2da24434eff57baa051f8c2e5301620ad
Liya Susan Biju <liyasusanbiju@gmail.com>
2022-12-30 03:27:35 +0530

    updated commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Events from './pages/events/Events';
 import Home from './pages/home/Home';
+import Joinus from './pages/joinus/Joinus';
 import Oppurtunities from './pages/oppurtunities/Oppurtunities';
 import Resources from './pages/resources/Resources';
 
@@ -14,6 +15,7 @@ function App() {
                 <Route path="/events" element={<Events />} />
                 <Route path="/resources" element={<Resources />} />
                 <Route path="/oppurtunities" element={<Oppurtunities />} />
+                <Route path="/joinus" element={<Joinus />} />
                 <Route path="*" element={<p>404</p>} />
             </Routes>
         </div>
---
+++ src/pages/joinus/Joinus.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './joinus.css';
+
+function Joinus() {
+    return <div></div>;
+}
+
+export default Joinus;
