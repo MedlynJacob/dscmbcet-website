62ba3979bb4b26d42b08636ba90fecda5d5625f7
Hipster16 <100425953+Hipster16@users.noreply.github.com>
2022-12-31 22:43:57 +0530

    Add oppurtunities page

--- src/pages/oppurtunities/Oppurtunities.tsx
+++ src/pages/oppurtunities/Oppurtunities.tsx
@@ -1,7 +1,30 @@
+import card from './components/card';
+import './Oppurtunities.css';
+
 function Oppurtunities() {
     return (
-        <div>
-            <h1>This is the oppurtunities page</h1>
+        <div id="oppurPage">
+            <div id="oppurPageName">
+                <div id="oppurHeader">
+                    <h1>logo</h1>
+                    <button>About us</button>
+                    <button>Events</button>
+                    <button>Resources</button>
+                    <button>Team</button>
+                    <button>Opputunties</button>
+                    <button>Join us</button>
+                </div>
+                <div>
+                    <h1>Opputunities</h1>
+                </div>
+            </div>
+            <div className="oppurContainer">
+                {card()}
+                {card()}
+                {card()}
+                {card()}
+                {card()}
+            </div>
         </div>
     );
 }
---
+++ src/pages/oppurtunities/components/card.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './card.css';
+function card() {
+    return (
+        <div className="cardContainer">
+            <h1 className="cardTittle">This is the Header area</h1>
+            <p className="cardDesc">This is the description area</p>
+            <div id="logo">
+                <p style={{ padding: 10, backgroundColor: '#1A1A1A', borderRadius: 20, color: 'white' }}>Design</p>
+                <p style={{ padding: 10, fontSize: 24, color: 'white' }}>
+                    12000<span style={{ fontSize: 14 }}>/mo</span>
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default card;
