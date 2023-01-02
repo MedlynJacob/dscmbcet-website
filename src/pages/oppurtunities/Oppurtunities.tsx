32d4b3a393f168e6b63a67b0df7196c734a3e985
Hipster16 <100425953+Hipster16@users.noreply.github.com>
2023-01-03 01:30:23 +0530

    oppurtunities updated

--- src/pages/oppurtunities/Oppurtunities.tsx
+++ src/pages/oppurtunities/Oppurtunities.tsx
@@ -1,28 +1,44 @@
 import Card from './components/card';
 import './Oppurtunities.css';
 import Navbar from '../../Components/Navbar/Navbar';
+import { useState } from 'react';
 
 function Oppurtunities() {
+    const [srcstr, setSrcStr] = useState('');
+    let cardArray = [
+        { header: 'is the header', desc: 'This is the description', domain: 'Domain' },
+        { header: 'This is the header', desc: 'This is the description', domain: 'Domain' },
+        { header: 'This is the header', desc: 'This is the description', domain: 'Domain' },
+        { header: 'This is the header', desc: 'This is the description', domain: 'Domain' },
+        { header: 'This is the header', desc: 'This is the description', domain: 'Domain' },
+        { header: 'This is the header', desc: 'This is the description', domain: 'Domain' },
+    ];
+    const filteredCardArray = cardArray.filter((name) => {
+        return name.header.toLowerCase().includes(srcstr);
+    });
     return (
         <div id="oppurPage">
             <Navbar />
             <div id="oppurPageName">
                 <h1 id="oppurHeader">Oppurtunities</h1>
                 <div className="oppurSearchBar">
-                    <input id="oppurSearchInput" type="search" placeholder="Search" />
-                    <button id="oppurSearchClearButton" type="reset">
+                    <input
+                        id="oppurSearchInput"
+                        type="text"
+                        placeholder="Search"
+                        value={srcstr}
+                        onChange={(e) => setSrcStr(e.target.value)}
+                    />
+                    <button id="oppurSearchClearButton" type="reset" onClick={() => setSrcStr('')}>
                         X
                     </button>
                 </div>
             </div>
             <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
                 <div className="oppurContainer">
-                    <Card vals={{ header: 'This is the header', desc: 'This is the description', domain: 'Domain' }} />
-                    <Card vals={{ header: 'This is the header', desc: 'This is the description', domain: 'Domain' }} />
-                    <Card vals={{ header: 'This is the header', desc: 'This is the description', domain: 'Domain' }} />
-                    <Card vals={{ header: 'This is the header', desc: 'This is the description', domain: 'Domain' }} />
-                    <Card vals={{ header: 'This is the header', desc: 'This is the description', domain: 'Domain' }} />
-                    <Card vals={{ header: 'This is the header', desc: 'This is the description', domain: 'Domain' }} />
+                    {filteredCardArray.map((name) => (
+                        <Card vals={name} />
+                    ))}
                 </div>
             </div>
         </div>
