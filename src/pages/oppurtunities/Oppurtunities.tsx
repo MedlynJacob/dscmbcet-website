a1b610d69fee21a3dd7e4906ef22c6b6d8cedafb
Hipster16 <100425953+Hipster16@users.noreply.github.com>
2023-01-02 19:00:32 +0530

    oppurtunities updated
    Co-authored-by: Ritin George <aurora-0025@users.noreply.github.com>

--- src/pages/oppurtunities/Oppurtunities.tsx
+++ src/pages/oppurtunities/Oppurtunities.tsx
@@ -5,19 +5,25 @@ import Navbar from '../../Components/Navbar/Navbar';
 function Oppurtunities() {
     return (
         <div id="oppurPage">
+            <Navbar />
             <div id="oppurPageName">
-                <Navbar />
                 <h1 id="oppurHeader">Oppurtunities</h1>
                 <div className="oppurSearchBar">
                     <input id="oppurSearchInput" type="search" placeholder="Search" />
+                    <button id="oppurSearchClearButton" type="reset">
+                        X
+                    </button>
                 </div>
             </div>
-            <div className="oppurContainer">
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+            <div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center' }}>
+                <div className="oppurContainer">
+                    <Card vals={{ header: 'This is the header', desc: 'This is the description', domain: 'Domain' }} />
+                    <Card vals={{ header: 'This is the header', desc: 'This is the description', domain: 'Domain' }} />
+                    <Card vals={{ header: 'This is the header', desc: 'This is the description', domain: 'Domain' }} />
+                    <Card vals={{ header: 'This is the header', desc: 'This is the description', domain: 'Domain' }} />
+                    <Card vals={{ header: 'This is the header', desc: 'This is the description', domain: 'Domain' }} />
+                    <Card vals={{ header: 'This is the header', desc: 'This is the description', domain: 'Domain' }} />
+                </div>
             </div>
         </div>
     );
