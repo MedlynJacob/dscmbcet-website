c647c3f19a1ea2373e790efaa8b6103c6e8b609d
Medlyn S Jacob <88194420+MedlynJacob@users.noreply.github.com>
2023-01-03 12:46:15 +0300

    Update Joinus.tsx

--- src/pages/joinus/Joinus.tsx
+++ src/pages/joinus/Joinus.tsx
@@ -13,20 +13,23 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 function Joinus() {
     return (
+       //Main div
         <div className='main' style={{backgroundImage:`url(${bg})`}}>
         
             <Navbar />
             <img src={joinusheader} alt="img" className="image1"></img>
+            
             <div className="centered">Join us</div>
               
+          
             <div className='yt'>
                 <div className='text'>
                     <h4>Find your zone, build your skills</h4>
                     <h6>Let’s grow together</h6>
                 </div>
-            <iframe className='video' width="560" height="315" src="https://www.youtube.com/embed/7o09af0BWiM" 
-            title="YouTube video player" frameBorder="0" 
-            data-allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <iframe className='video' width="560" height="315" src="https://www.youtube.com/embed/7o09af0BWiM" 
+                title="YouTube video player" frameBorder="0" 
+                data-allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
             
             <button className='Join_button'><p className='button_text'>JOIN US</p></button>
